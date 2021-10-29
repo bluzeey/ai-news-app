4c50e197141cba755fe0f08e6bdee589c271a4cf
Sahil Maheshwari <sahilm1711@gmail.com>
2021-10-29 19:03:02 +0530

    Debugging Alan AI studio

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React,{useEffect} from 'react'
+import React,{useState,useEffect} from 'react'
 import alanBtn from '@alan-ai/alan-sdk-web'
 
 const alanKey='da09d2951242e3e7b8bd8c6d439e94e62e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
+    const {newsArticles,setNewsArticles}=useState([])
+
     useEffect(()=>{
         alanBtn({
             key:alanKey,
             onCommand:({command,articles})=>{
-              if(command==='newHeadlines'){
-                  console.log(articles)
+              if(command==='newsHeadlines'){
+                  setNewsArticles(articles)
               }
             }
         })
