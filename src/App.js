3c9e04029cc1bf9141bb74ab8b8e686a7024ad3b
Sahil Maheshwari <sahilm1711@gmail.com>
2021-11-01 07:29:18 +0530

    Updating alan news

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from 'react'
 import alanBtn from '@alan-ai/alan-sdk-web'
-
+import NewsCards from './components/NewsCards/NewsCards';
 const alanKey='da09d2951242e3e7b8bd8c6d439e94e62e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
     const {newsArticles,setNewsArticles}=useState([])
@@ -10,14 +10,15 @@ const App = () => {
             key:alanKey,
             onCommand:({command,articles})=>{
               if(command==='newsHeadlines'){
-                  setNewsArticles(articles)
-              }
+                  setNewsArticles(articles);
+              };
             }
         })
     },[])
     return (
         <div>
             <h1>Alan AI News Application</h1>
+              <NewsCards articles={newsArticles}/>
         </div>
     )
 }
